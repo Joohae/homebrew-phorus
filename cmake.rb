1deb9caca5370aac3fc86f235f95353582f73659
Joohae Kim <joohae@phorus.com>
2017-04-12 15:14:44 -0700

    add cmake

---
+++ cmake.rb
@@ -0,0 +1,61 @@
+# Documentation: http://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Cmake < Formula
+  desc ""
+  homepage ""
+  url "https://cmake.org/files/v3.7/cmake-3.7.1.tar.gz"
+  version "3.7.1"
+  sha256 "449a5bce64dbd4d5b9517ebd1a1248ed197add6ad27934478976fd5f1f9330e1"
+
+  # depends_on "cmake" => :build
+  option "without-docs", "Don't build man pages"
+  option "with-completion", "Install Bash completion (Has potential problems with system bash)"
+
+  depends_on "sphinx-doc" => :build if build.with? "docs"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --no-system-libs
+      --parallel=#{ENV.make_jobs}
+      --datadir=/share/cmake
+      --docdir=/share/doc/cmake
+      --mandir=/share/man
+      --system-zlib
+      --system-bzip2
+    ]
+
+    # https://github.com/Homebrew/legacy-homebrew/issues/45989
+    if MacOS.version <= :lion
+      args << "--no-system-curl"
+    else
+      args << "--system-curl"
+    end
+
+    if build.with? "docs"
+      # There is an existing issue around macOS & Python locale setting
+      # See https://bugs.python.org/issue18378#msg215215 for explanation
+      ENV["LC_ALL"] = "en_US.UTF-8"
+      args << "--sphinx-man" << "--sphinx-build=#{Formula["sphinx-doc"].opt_bin}/sphinx-build"
+    end
+
+    system "./bootstrap", *args
+    system "make"
+    system "make", "install"
+
+    if build.with? "completion"
+      cd "Auxiliary/bash-completion/" do
+        bash_completion.install "ctest", "cmake", "cpack"
+      end
+    end
+
+    elisp.install "Auxiliary/cmake-mode.el"
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write("find_package(Ruby)")
+    system bin/"cmake", "."
+  end
+end
